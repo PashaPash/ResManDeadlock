a2a166f7731a0bd50abbfe9247311e7b8ef62574
Pavel Pochobut <pavel.pochobut@gmail.com>
2015-02-04 22:38:08 +0300

    Added Deadlock sample

---
+++ ResManDeadlock/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Resources;
+using System.Threading.Tasks;
+
+namespace ResManDeadlock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var resManager = new ResourceManager("ResManDeadlock.Forms", typeof(Forms).Assembly);
+                var t2 = Task.Run(() =>
+                {
+                    foreach (var r in resManager.GetResourceSet(CultureInfo.CurrentCulture, true, true))
+                    {
+                    
+                    }
+                });
+
+                var t1 = Task.Run(() =>
+                {
+                    var rs = resManager.GetResourceSet(CultureInfo.CurrentCulture, true, true);
+                    rs.GetString("formsEdit_description");
+                });
+
+                Task.WaitAll(t1, t2);
+
+                Console.Write(".");
+            }
+        }
+    }
+}
